91e7b81257989df7c8e7b2511e3d09855ef0060b
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-19 11:19:14 +0300

    Added write_statistics_to_files, some refactoring

--- pull_messages_from_vk.py
+++ pull_messages_from_vk.py
@@ -4,14 +4,8 @@ import settings
 import time
 import os
 
-# TODO: записывать сообщения и подобную информацию в базу
-# TODO: запись контекстным менеджером? (актуально с базой?)
-# TODO: можно добавлять статистику в словари
-'''
-При инициализации класса нужно создавать два экземпляра (свой и чужой).
-Инстанс должен иметь имя папки, создавать их при инициализации.
-'''
 
+# TODO: записывать сообщения и подобную информацию в базу
 
 class VkUser:
     """
@@ -20,86 +14,78 @@ class VkUser:
 
     def __init__(self, user_id):
         self.user_id = user_id
-
-        self.statistics = {
-            'message_counter': 0,
-            'sticker_counter': 0,
-            'link_counter': 0,
-        }
-
         self.FOLDER_NAME = f'Сообщения от {self.user_id}'
 
-        if not os.path.exists(self.FOLDER_NAME):
-            os.mkdir(self.FOLDER_NAME)
+        self.statistics = {
+            'messages': [],
+            'stickers': [],
+            'links': [],
+            'gifs': [],
+            'docs': [],
+            'photos': [],
+            'videos_counter': 0,
+            'audio_messages': [],
+            'walls_counter': 0,
+            'audios': [],
+            'gifts': [],
+            'calls_counter': 0,
+            'graffiti': [],
+            'stories_counter': 0
+        }
 
     def parse_message(self, message):
-
         if message['text']:
             text = message['text'].replace('\n', ' ')
-            f = open(fr'{self.FOLDER_NAME}\Сообщения.txt', 'a', encoding='UTF-8')
-            f.write(text + '\n')
-
+            self.statistics['messages'].append(text)
         if message['attachments']:
             for attachment in message['attachments']:
                 if attachment['type'] == 'sticker':
-                    f = open(fr'{self.FOLDER_NAME}\Стикеры.txt', 'a', encoding='UTF-8')
-                    f.write(str(attachment['sticker']['sticker_id']) + '\n')
-                    f.close()
-
+                    self.statistics['stickers'].append(attachment['sticker']['sticker_id'])
                 elif attachment['type'] == 'link':
-                    f = open(fr'{self.FOLDER_NAME}\Ссылки.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['link']['url'] + '\n')
-                    f.close()
+                    self.statistics['links'].append(attachment['link']['url'])
                 elif attachment['type'] == 'doc':
                     if attachment['doc']['ext'] == 'gif':
-                        f = open(fr'{self.FOLDER_NAME}\Гифки.txt', 'a', encoding='UTF-8')
-                        f.write(attachment['doc']['url'] + '\n')
-                        f.close()
+                        self.statistics['gifs'].append(attachment['doc']['url'])
                     else:
-                        f = open(fr'{self.FOLDER_NAME}\Документы.txt', 'a', encoding='UTF-8')
-                        f.write(attachment['doc']['title'] + ' ' + attachment['doc']['url'] + '\n')
-                        f.close()
+                        self.statistics['docs'].append(attachment['doc']['title'] + ' ' + attachment['doc']['url'])
                 elif attachment['type'] == 'photo':
-                    f = open(fr'{self.FOLDER_NAME}\Фото.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['photo']['sizes'][-1]['url'] + '\n')
-                    f.close()
+                    self.statistics['photos'].append(attachment['photo']['sizes'][-1]['url'])
                 elif attachment['type'] == 'video':
-                    f = open(fr'{self.FOLDER_NAME}\Видео.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['video']['title'] + '\n')
-                    f.close()
+                    self.statistics['videos_counter'] += 1
                 elif attachment['type'] == 'audio_message':
                     audio_date_in_seconds = message['date']
                     date_time = datetime.fromtimestamp(audio_date_in_seconds)
                     date_time = date_time.strftime("%d.%m.%Y ")
-                    f = open(fr'{self.FOLDER_NAME}\Аудиосообщения.txt', 'a', encoding='UTF-8')
-                    f.write(date_time + str(attachment['audio_message']['duration']) + '\n')
-                    f.close()
+                    self.statistics['audio_messages'].append(
+                        date_time + ' ' + str(attachment['audio_message']['duration']))
                 elif attachment['type'] == 'wall':
-                    f = open(fr'{self.FOLDER_NAME}\Записи на стене.txt', 'a', encoding='UTF-8')
-                    f.write('1\n')
-                    f.close()
+                    self.statistics['walls_counter'] += 1
                 elif attachment['type'] == 'audio':
-                    f = open(fr'{self.FOLDER_NAME}\Аудиозаписи.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['audio']['title'] + '\n')
-                    f.close()
+                    self.statistics['audios'].append(attachment['audio']['title'])
                 elif attachment['type'] == 'gift':
-                    f = open(fr'{self.FOLDER_NAME}\Подарки.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['gift']['thumb_256'] + '\n')
-                    f.close()
+                    self.statistics['gifts'].append(attachment['gift']['thumb_256'])
                 elif attachment['type'] == 'call':
-                    f = open(fr'{self.FOLDER_NAME}\Звонки.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['call']['state'] + '\n')
-                    f.close()
+                    self.statistics['calls_counter'] += 1
                 elif attachment['type'] == 'graffiti':
-                    f = open(fr'{self.FOLDER_NAME}\Граффити.txt', 'a', encoding='UTF-8')
-                    f.write(attachment['graffiti']['url'] + '\n')
-                    f.close()
+                    self.statistics['graffiti'].append(attachment['graffiti']['url'])
                 elif attachment['type'] == 'story':
-                    f = open(fr'{self.FOLDER_NAME}\Сториз.txt', 'a', encoding='UTF-8')
-                    f.write('1\n')
-                    f.close()
+                    self.statistics['stories_counter'] += 1
                 else:
                     print(attachment)
+
+    def print_statistics(self):
+        print(f'Статистика от пользователя id{self.user_id}:\n{len(self.statistics["messages"])}')
+
+    def write_statistics_to_files(self):
+        if not os.path.exists(self.FOLDER_NAME):
+            os.mkdir(self.FOLDER_NAME)
+        for filename in self.statistics.keys():
+            if self.statistics[filename]:
+                with open(f'{self.FOLDER_NAME}\\{filename}.txt', 'a', encoding='UTF-8') as f:
+                    try:
+                        f.write('\n'.join([str(message) for message in self.statistics[f'{filename}']]))
+                    except TypeError:
+                        f.write(str(self.statistics[f'{filename}']))
 
 
 class VkParser:
@@ -109,7 +95,7 @@ class VkParser:
     """
 
     def __init__(self, dialogue_id, login=settings.login, password=settings.password, two_factor=True):
-        self.user_id = dialogue_id
+        self.dialogue_id = dialogue_id
         self.login = login
         self.password = password
         self.two_factor = two_factor
@@ -124,12 +110,12 @@ class VkParser:
         self.messages_api = MessagesAPI(login=self.login, password=self.password, two_factor=self.two_factor,
                                         cookies_save_path='sessions/')
 
-        self.TOTAL_MESSAGES = self.messages_api.method('messages.getHistory', user_id=self.user_id)['count']
+        self.TOTAL_MESSAGES = self.messages_api.method('messages.getHistory', user_id=self.dialogue_id)['count']
 
     def run(self):
         number_of_scan_cycles = self.TOTAL_MESSAGES // self.SCAN_MESSAGES_PER_CALL + 1
         for _ in range(number_of_scan_cycles):
-            json_messages = self.messages_api.method('messages.getHistory', user_id=self.user_id,
+            json_messages = self.messages_api.method('messages.getHistory', user_id=self.dialogue_id,
                                                      count=self.SCAN_MESSAGES_PER_CALL,
                                                      rev=-1, offset=self.offset_scanned_messages)
             for message in json_messages['items']:
@@ -148,8 +134,8 @@ class VkParser:
 
 
 if __name__ == '__main__':
-    my_id = 22491953
-    friend_id = 183145350
+    my_id = 123
+    friend_id = 123
     my_vk = VkUser(my_id)
     vk_friend = VkUser(friend_id)
 
