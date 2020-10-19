a273313db11b63de887f51e0c11b9864725ae1b4
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-19 13:51:11 +0300

    Added print_statistics and some improvements

--- pull_messages_from_vk.py
+++ pull_messages_from_vk.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from datetime import datetime
 from vk_messages import MessagesAPI
 import settings
@@ -14,20 +15,32 @@ class VkUser:
 
     def __init__(self, user_id):
         self.user_id = user_id
-        self.FOLDER_NAME = f'Сообщения от {self.user_id}'
+
+
+        if not os.path.exists('sessions/'):
+            os.mkdir('sessions/')
+        # Start session
+        self.messages_api = MessagesAPI(login=settings.login, password=settings.password, two_factor=True,
+                                        cookies_save_path='sessions/')
+        self.first_name = self.messages_api.method('users.get', user_ids=self.user_id, name_case='gen')[0]['first_name']
+        self.last_name = self.messages_api.method('users.get', user_ids=self.user_id, name_case='gen')[0]['last_name']
+        self.FOLDER_NAME = f'Сообщения от {self.first_name} {self.last_name}'
 
         self.statistics = {
             'messages': [],
             'stickers': [],
+            'stickers_counter': Counter(),
             'links': [],
             'gifs': [],
             'docs': [],
             'photos': [],
             'videos_counter': 0,
             'audio_messages': [],
+            'duration_of_audio_messages': [],
             'walls_counter': 0,
             'audios': [],
             'gifts': [],
+            'gifted_stickers': 0,
             'calls_counter': 0,
             'graffiti': [],
             'stories_counter': 0
@@ -41,6 +54,7 @@ class VkUser:
             for attachment in message['attachments']:
                 if attachment['type'] == 'sticker':
                     self.statistics['stickers'].append(attachment['sticker']['sticker_id'])
+                    self.statistics['stickers_counter'][attachment['sticker']['sticker_id']] += 1
                 elif attachment['type'] == 'link':
                     self.statistics['links'].append(attachment['link']['url'])
                 elif attachment['type'] == 'doc':
@@ -58,12 +72,16 @@ class VkUser:
                     date_time = date_time.strftime("%d.%m.%Y ")
                     self.statistics['audio_messages'].append(
                         date_time + ' ' + str(attachment['audio_message']['duration']))
+                    self.statistics['duration_of_audio_messages'].append(attachment['audio_message']['duration'])
                 elif attachment['type'] == 'wall':
                     self.statistics['walls_counter'] += 1
                 elif attachment['type'] == 'audio':
                     self.statistics['audios'].append(attachment['audio']['title'])
                 elif attachment['type'] == 'gift':
                     self.statistics['gifts'].append(attachment['gift']['thumb_256'])
+                    if 'sticker' in attachment['gift']['thumb_256'] or '10001' in attachment['gift']['thumb_256'] \
+                            or '10002' in attachment['gift']['thumb_256']:
+                        self.statistics['gifted_stickers'] += 1
                 elif attachment['type'] == 'call':
                     self.statistics['calls_counter'] += 1
                 elif attachment['type'] == 'graffiti':
@@ -74,7 +92,47 @@ class VkUser:
                     print(attachment)
 
     def print_statistics(self):
-        print(f'Статистика от пользователя id{self.user_id}:\n{len(self.statistics["messages"])}')
+        print('@' * 60)
+        print(f'Статистика от {self.first_name} {self.last_name}:')
+        if self.statistics['messages']:
+            print(f'Отправлено сообщений: {len(self.statistics["messages"])}')
+        if self.statistics['stickers']:
+            print(f'Отправлено стикеров: {len(self.statistics["stickers"])}')
+            print(f'Самые популярные стикеры:')
+            for sticker_id, count in self.statistics["stickers_counter"].most_common(3):
+                print(f'https://vk.com/sticker/1-{sticker_id}-512b использовался {count} раз')
+        if self.statistics['links']:
+            print(f'Отправлено ссылок: {len(self.statistics["links"])}')
+        if self.statistics['gifs']:
+            print(f'Отправлено гифок: {len(self.statistics["gifs"])}')
+        if self.statistics['docs']:
+            print(f'Отправлено документов: {len(self.statistics["docs"])}')
+        if self.statistics['photos']:
+            print(f'Отправлено изображений: {len(self.statistics["photos"])}')
+        if self.statistics['videos_counter']:
+            print(f'Отправлено видео: {self.statistics["videos_counter"]}')
+        if self.statistics['audio_messages']:
+            print(f'Отправлено аудиосообщений: {len(self.statistics["audio_messages"])}')
+            print(f'Продолжительность аудиосообщений: {sum(self.statistics["duration_of_audio_messages"])} сек.')
+            if len(self.statistics['audio_messages']) > 1:
+                print(f'Самое долгое аудиосообщение длилось {max(self.statistics["duration_of_audio_messages"])} сек.')
+        if self.statistics['walls_counter']:
+            print(f'Отправлено записей со стен: {self.statistics["walls_counter"]}')
+        if self.statistics['audios']:
+            print(f'Отправлено песен: {len(self.statistics["audios"])}')
+        if self.statistics['gifts']:
+            print(f'Отправлено подарков: {len(self.statistics["gifts"])}', end='')
+            if self.statistics['gifted_stickers']:
+                print(f', из которых стикеров - {self.statistics["gifted_stickers"]}')
+            else:
+                print()
+        if self.statistics['calls_counter']:
+            print(f'Звонков совершено: {self.statistics["calls_counter"]}')
+        if self.statistics['graffiti']:
+            print(f'Отправлено граффити: {len(self.statistics["graffiti"])}')
+        if self.statistics['stories_counter']:
+            print(f'Отправлено историй: {self.statistics["stories_counter"]}')
+        print()
 
     def write_statistics_to_files(self):
         if not os.path.exists(self.FOLDER_NAME):
@@ -134,10 +192,15 @@ class VkParser:
 
 
 if __name__ == '__main__':
-    my_id = 123
-    friend_id = 123
+    my_id = 22491953
+    friend_id = 17026394
     my_vk = VkUser(my_id)
     vk_friend = VkUser(friend_id)
 
     vk_parse = VkParser(dialogue_id=friend_id)
     vk_parse.run()
+    my_vk.print_statistics()
+    vk_friend.print_statistics()
+
+    my_vk.write_statistics_to_files()
+    vk_friend.write_statistics_to_files()
