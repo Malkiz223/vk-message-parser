9aa611cbae738151d8c9d4b355f7ca4ccf3876bb
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-24 19:49:17 +0300

    Removed calling VkUser instances from VkParser. Made the print_progress_to_console and parse_messages functions.

--- pull_messages_from_vk.py
+++ pull_messages_from_vk.py
@@ -6,8 +6,6 @@ import time
 import os
 
 
-# TODO: записывать сообщения и подобную информацию в базу
-
 class Vk:
     """
     Суперкласс, имеющий доступ к API VK, хранящий сессию в отдельной папочке
@@ -107,7 +105,7 @@ class VkUser(Vk):
         Печатает в консоль отформатированную статистику, собранную в self.statistics
         :return: None
         """
-        print('@' * 60)
+        print('@' * 34)
         print(f'Статистика от {self.first_name} {self.last_name}:')
         if self.statistics['messages']:
             print(f'Отправлено сообщений: {len(self.statistics["messages"])}')
@@ -172,11 +170,11 @@ class VkParser(Vk):
 
     def __init__(self, friend_id, messages_api=Vk.messages_api):
         self.friend_id = friend_id
+        self.all_json_messages = []
         self.SCAN_MESSAGES_PER_CALL = 200
         self.offset_scanned_messages = 0
         self.now_scanned = self.SCAN_MESSAGES_PER_CALL + self.offset_scanned_messages
-        self.TOTAL_MESSAGES = messages_api.method('messages.getHistory', user_id=self.friend_id)['count']
-        self.NUMBER_OF_SCAN_CYCLES = self.TOTAL_MESSAGES // self.SCAN_MESSAGES_PER_CALL + 1
+        self.total_messages = messages_api.method('messages.getHistory', user_id=self.friend_id)['count']
 
     def parse_messages(self):
         """
@@ -184,24 +182,26 @@ class VkParser(Vk):
         При каждом вызове offset смещается, позволяя запарсить следующие 200 сообщений.
         :return: None
         """
-        json_messages = self.messages_api.method('messages.getHistory', user_id=self.friend_id,
-                                                 count=self.SCAN_MESSAGES_PER_CALL,
-                                                 rev=-1, offset=self.offset_scanned_messages)
-        for message in json_messages['items']:
-            if message['from_id'] == friend_id:
-                vk_friend.collect_statistics(message)
-            else:
-                my_vk.collect_statistics(message)
-        self.offset_scanned_messages += self.SCAN_MESSAGES_PER_CALL
+        try:
+            json_messages = self.messages_api.method('messages.getHistory', user_id=self.friend_id,
+                                                     count=self.SCAN_MESSAGES_PER_CALL,
+                                                     rev=-1, offset=self.offset_scanned_messages)
+            for message in json_messages['items']:
+                self.all_json_messages.append(message)
+            self.offset_scanned_messages += self.SCAN_MESSAGES_PER_CALL
+        except AttributeError as err:
+            print(f'Поймал {err}, продолжаю работу. Сообщения не потеряны, всё стабильно!')
+        except BaseException as err:
+            print(f'Поймали {err}')
 
     def print_progress_to_console(self):
         """
         Печатает в консоль прогресс сканирования сообщений.
         :return: None
         """
-        if self.now_scanned > self.TOTAL_MESSAGES:
-            self.now_scanned = self.TOTAL_MESSAGES
-        print(f'Просканировано {self.now_scanned} из {self.TOTAL_MESSAGES} сообщений')
+        if self.now_scanned > self.total_messages:
+            self.now_scanned = self.total_messages
+        print(f'Просканировано {self.now_scanned} из {self.total_messages} сообщений')
         self.now_scanned = self.SCAN_MESSAGES_PER_CALL + self.offset_scanned_messages
 
     def run(self):
@@ -209,18 +209,21 @@ class VkParser(Vk):
         Основная функция, запускает парсинг сообщений, выжидая между парсингом определённое время и печатая статистику.
         :return: None
         """
-        for _ in range(self.NUMBER_OF_SCAN_CYCLES):
+        while self.offset_scanned_messages <= self.total_messages:
             self.parse_messages()
             self.print_progress_to_console()
-            time.sleep(0.2)
+            time.sleep(0.1)  # Без задержки работает стабильно, но страшно
 
 
 if __name__ == '__main__':
-    my_id = 123
-    friend_id = 456
-
-    my_vk = VkUser(my_id)
+    friend_id = 123
+    my_vk = VkUser(settings.my_id)
     vk_friend = VkUser(friend_id)
 
-    vk_parse = VkParser(friend_id)
-    vk_parse.run()
+    vk_parser = VkParser(friend_id)
+    vk_parser.run()
+    for message in vk_parser.all_json_messages:
+        if message['from_id'] == friend_id:
+            vk_friend.collect_statistics(message)
+        else:
+            my_vk.collect_statistics(message)
