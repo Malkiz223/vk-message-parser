fe84142df547dc95aa303d0910d2ec9d2dedaa9c
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-25 18:22:34 +0300

    Uppercase constants

--- vk.py
+++ vk.py
@@ -7,10 +7,10 @@ import settings
 
 class Vk:
     """
-    Суперкласс, имеющий доступ к API VK, хранящий сессию в отдельной папочке
+    Суперкласс, имеющий доступ к API VK, хранящий сессию в отдельной папке
     """
     if not os.path.exists('sessions/'):
         os.mkdir('sessions/')
 
-    messages_api = MessagesAPI(login=settings.login, password=settings.password, two_factor=True,
+    messages_api = MessagesAPI(login=settings.LOGIN, password=settings.PASSWORD, two_factor=True,
                                cookies_save_path='sessions/')
