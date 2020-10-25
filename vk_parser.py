6fc5c6858ed68a9848e1ee96a24dc9baad058cfd
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-25 18:23:29 +0300

    Renamed functions

--- vk_parser.py
+++ vk_parser.py
@@ -6,24 +6,26 @@ from vk import Vk
 class VkParser(Vk):
     """
     Сканирует сообщения с указанным пользователем.
+    :arg friend_id (int): ID собеседника в VK
+    :arg messages_api (class): API VK
     """
 
     def __init__(self, friend_id, messages_api=Vk.messages_api):
-        self.friend_id = friend_id
+        self.FRIEND_ID = friend_id
         self.all_json_messages = []
         self.SCAN_MESSAGES_PER_CALL = 200
         self.offset_scanned_messages = 0
         self.now_scanned = self.SCAN_MESSAGES_PER_CALL + self.offset_scanned_messages
-        self.total_messages = messages_api.method('messages.getHistory', user_id=self.friend_id)['count']
+        self.total_messages = messages_api.method('messages.getHistory', user_id=self.FRIEND_ID)['count']
 
-    def parse_messages(self):
+    def get_messages_from_vk(self):
         """
         Обращается к API VK и вытягивает 200 сообщений.
         При каждом вызове offset смещается, позволяя запарсить следующие 200 сообщений.
         :return: None
         """
         try:
-            json_messages = self.messages_api.method('messages.getHistory', user_id=self.friend_id,
+            json_messages = self.messages_api.method('messages.getHistory', user_id=self.FRIEND_ID,
                                                      count=self.SCAN_MESSAGES_PER_CALL,
                                                      rev=-1, offset=self.offset_scanned_messages)
             for message in json_messages['items']:
@@ -34,7 +36,7 @@ class VkParser(Vk):
         except BaseException as err:
             print(f'Поймали {err}')
 
-    def print_progress_to_console(self):
+    def print_parsing_progress_to_console(self):
         """
         Печатает в консоль прогресс сканирования сообщений.
         :return: None
@@ -50,6 +52,6 @@ class VkParser(Vk):
         :return: None
         """
         while self.offset_scanned_messages <= self.total_messages:
-            self.parse_messages()
-            self.print_progress_to_console()
-            time.sleep(0.1)  # Без задержки работает стабильно, но страшно
+            self.get_messages_from_vk()
+            self.print_parsing_progress_to_console()
+            time.sleep(0.1)  # Без задержки работает стабильно, но вдруг начнут блокировать запросы
