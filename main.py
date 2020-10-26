26843d34d8c827c05fe30a30e09038de01237f91
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-26 07:14:12 +0300

    Change comment and variable name MY_ID

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from vk_user import VkUser
 
 
 def main(FRIEND_ID):
-    my_vk = VkUser(settings.MY_ID)
+    my_vk = VkUser(settings.MY_VK_ID)
     vk_friend = VkUser(FRIEND_ID)
 
     vk_parser = VkParser(FRIEND_ID)
     vk_parser.run()
 
-    # Собирает статистику сообщений, передавая её в инстансы VkUser
+    # Передаёт каждое сообщение в конкретный инстанс VkUser, собирая статистику
     for message in vk_parser.all_json_messages:
         if message['from_id'] == FRIEND_ID:
             vk_friend.collect_statistics(message)
