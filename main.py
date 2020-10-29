6ee9fa846c8af200f01b38ce676c657a3ffff22f
Dmitry Agafonov <malkiz@yandex.ru>
2020-10-29 15:54:36 +0300

    Create a quick start template

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from vk_user import VkUser
 def main(FRIEND_ID):
     my_vk = VkUser(settings.MY_VK_ID)
     vk_friend = VkUser(FRIEND_ID)
-
     vk_parser = VkParser(FRIEND_ID)
     vk_parser.run()
-
     # Передаёт каждое сообщение в конкретный инстанс VkUser, собирая статистику
     for message in vk_parser.all_json_messages:
         if message['from_id'] == FRIEND_ID:
@@ -18,7 +16,15 @@ def main(FRIEND_ID):
         else:
             my_vk.collect_statistics(message)
 
+    # Раскомментируй необходимый код
+    # vk_friend.print_statistics_to_console()
+    # vk_friend.print_most_popular_words(15)
+    # vk_friend.write_statistics_to_files()
+    # my_vk.print_statistics_to_console()
+    # my_vk.print_most_popular_words(20)
+    # my_vk.write_statistics_to_files()
+
 
 if __name__ == '__main__':
-    FRIEND_ID = 123
+    FRIEND_ID =  # int
     main(FRIEND_ID)
