c8adfe0faf0915de474ce51e4d1ec352fc5488ed
Dmitry Agafonov <malkiz@yandex.ru>
2021-07-22 04:03:48 +0300

    Add some methods to save users in database

--- vk_parser.py
+++ vk_parser.py
@@ -36,7 +36,30 @@ class VkParser:
         self.offset_scanned_messages = 0
         self.count_messages_was_printed = 0
         self.now_scanned = self.SCAN_MESSAGES_PER_CALL + self.offset_scanned_messages
-        self.total_messages = self.vk_api.method('messages.getHistory', user_id=self.FRIEND_ID)['count']
+        self.save_to_db_methods = {
+            'photo': self._save_photo_to_db,
+            'sticker': self._save_sticker_to_db,
+            'link': self._save_link_to_db,
+            'video': self._save_video_to_db,
+            'doc': self._save_doc_to_db,
+            'audio': self._save_audio_to_db,
+            'audio_message': self._save_audio_message_to_db,
+            'call': self._save_call_to_db,
+            'gift': self._save_gift_to_db,
+            'wall': self._save_wall_to_db,
+            'graffiti': self._save_graffiti_to_db,
+            'story': self._save_story_to_db,
+        }
+
+        # получаем self.(friend_id, friend_url_nickname, friend_first_name, friend_last_name) и аналогично с self.my_...
+        self._get_users_name_and_id_data(friend_id)
+        self._save_users_to_db()
+        while True:
+            try:
+                self.total_messages = self.vk_api.method('messages.getHistory', user_id=self.friend_id)['count']
+                break
+            except ConnectionError:  # иногда сервер ВК банит частые запросы и скрипт падает
+                time.sleep(0.2)
 
     def get_messages_from_vk(self) -> None:
         """
@@ -66,11 +89,27 @@ class VkParser:
 
     def save_messages_to_db(self) -> None:
         for message in self.messages:
-
-            message_id = message['id']
-            if message.get('attachments'):
+            message_id: int = message['id']
+            date_gmt: datetime = datetime.fromtimestamp(message['date'])
+            from_id: int = message['from_id']
+            chat_id: int = message['peer_id']
+            conversation_message_id: int = message.get('conversation_message_id')
+            text: str = message['text']
+            self.cursor.execute(
+                'INSERT INTO messages (message_id, date_gmt, from_id, chat_id, conversation_message_id, text) '
+                'VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (message_id) DO NOTHING',
+                (message_id, date_gmt, from_id, chat_id, conversation_message_id, text,))
+            if self.cursor.statusmessage == 'INSERT 0 1':  # если значение вставлено успешно
                 for attachment in message.get('attachments'):
-                    self.save_to_db_methods[attachment['type']](message_id, attachment)
+                    attachment_type: str = attachment['type']
+                    if attachment_type == 'market':
+                        continue
+                    try:
+                        # в зависимости от типа вложения запускается нужный метод, см. __init__.self.save_to_db_methods
+                        self.save_to_db_methods[attachment_type](message['id'], attachment[attachment_type])
+                    except TypeError:
+                        print(f'[ERR] Не вставили значение в базу, пропускаем: {attachment}')
+        self.connection.commit()
         del self.messages[:]
         return True
 
@@ -151,7 +190,17 @@ class VkParser:
                 self.my_last_name = my_user_data['last_name']
                 break
             except ConnectionError:
-                time.sleep(0.1)
+                time.sleep(0.2)
+        while True:
+            try:
+                friend_user_data = self.vk_api.method('users.get', user_ids=input_id, fields='screen_name')[0]
+                self.friend_id = friend_user_data['id']
+                self.friend_url_nickname = friend_user_data['screen_name']
+                self.friend_first_name = friend_user_data['first_name']
+                self.friend_last_name = friend_user_data['last_name']
+                break
+            except ConnectionError:
+                time.sleep(0.2)
 
     def run(self) -> None:
         """
