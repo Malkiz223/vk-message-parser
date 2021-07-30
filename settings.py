2ddb92aa092e5ea38aefdbdc3df4b9f4e4462348
Dmitry Agafonov <malkiz@yandex.ru>
2021-07-31 02:40:44 +0300

    Add comments

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ VK_LOGIN: str = os.getenv('VK_LOGIN')  # 'admin@gmail.com'
 VK_PASSWORD: str = os.getenv('VK_PASSWORD')  # 'qwerty123'
 TWO_FACTOR: bool = True  # False, если у вас отсутствует двухфакторная аутентификация
 
-# Настройки PostgreSQL, если он есть
-postgres_user: str = 'postgres'
-postgres_password: str = os.getenv('POSTGRESQL_PASSWORD')
-postgres_database: str = 'vk_messages'
-postgres_host: str = '127.0.0.1'
-postgres_port: int = 5432
+# Настройки PostgreSQL
+postgres_user: str = 'postgres'  # пользователь по умолчанию
+postgres_password: str = os.getenv('POSTGRESQL_PASSWORD')  # пароль для подключения к пользователю
+postgres_database: str = 'vk_messages'  # название созданной вами базы
+postgres_host: str = '127.0.0.1'  # localhost, ip по умолчанию
+postgres_port: int = 5432  # порт по умолчанию
