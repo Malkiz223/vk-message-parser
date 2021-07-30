3bb05b468a6c004fd8fd05089576389f2bf6b4d3
Dmitry Agafonov <malkiz@yandex.ru>
2021-07-30 08:57:53 +0300

    Add PostgreSQL settings

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
 import os
 
-VK_LOGIN = os.getenv('VK_LOGIN')  # str, 'admin@gmail.com' as an example
-VK_PASSWORD = os.getenv('VK_PASSWORD')  # str, 'qwerty123' as an example
-TWO_FACTOR = False  # bool, do you have two-factor authentication
+VK_LOGIN: str = os.getenv('VK_LOGIN')  # 'admin@gmail.com'
+VK_PASSWORD: str = os.getenv('VK_PASSWORD')  # 'qwerty123'
+TWO_FACTOR: bool = True  # False, если у вас отсутствует двухфакторная аутентификация
+
+# Настройки PostgreSQL, если он есть
+postgres_user: str = 'postgres'
+postgres_password: str = os.getenv('POSTGRESQL_PASSWORD')
+postgres_database: str = 'vk_messages'
+postgres_host: str = '127.0.0.1'
+postgres_port: int = 5432
