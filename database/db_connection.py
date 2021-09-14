fe12b002b3ff670db3e484ff60a7bd498f29c449
Dmitry Agafonov <malkiz@yandex.ru>
2021-09-14 03:22:21 +0300

    Make constant an uppercase

--- database/db_connection.py
+++ database/db_connection.py
@@ -3,14 +3,14 @@ import sys
 import psycopg2
 from psycopg2._psycopg import connection as _connect, cursor as _cursor  # нужны для автодополнения в IDE
 
-from settings import postgres_user, postgres_password, postgres_dbname, postgres_host, postgres_port
+from settings import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DBNAME, POSTGRES_HOST, POSTGRES_PORT
 
 try:
-    connect: _connect = psycopg2.connect(user=postgres_user, password=postgres_password,
-                                         dbname=postgres_dbname, host=postgres_host, port=postgres_port)
+    connect: _connect = psycopg2.connect(user=POSTGRES_USER, password=POSTGRES_PASSWORD,
+                                         dbname=POSTGRES_DBNAME, host=POSTGRES_HOST, port=POSTGRES_PORT)
     cursor: _cursor = connect.cursor()  # аннотация типов у connection и cursor нужна для автодополнения в IDE
 except psycopg2.OperationalError:
-    sys.exit(f'Не смогли подключиться к базе PostgreSQL с названием {postgres_dbname}')
+    sys.exit(f'Не смогли подключиться к базе PostgreSQL с названием {POSTGRES_DBNAME}')
 
 
 def create_tables_if_not_exists(connect, cursor):
